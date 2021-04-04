dd46b51bf1ca483242c0d7ba3f3fe1a8dd2cb447
Przemek Nosek <przemo99.10@gmail.com>
2021-04-04 18:32:28 +0200

    done

---
+++ src/main/java/pl/java/permcheck/Solution.java
@@ -0,0 +1,25 @@
+package pl.java.permcheck;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] A = {4,1,3,2};
+        System.out.println(Solution.solution(A));
+    }
+
+    public static int solution(int[] A) {
+        Arrays.sort(A);
+
+        if (A[A.length-1] != A.length)
+            return 0;
+
+        int visited = 0;
+        for (int j : A) {
+            visited++;
+            if (j != visited)
+                return 0;
+        }
+        return 1;
+    }
+}
